3b67919c44d99de528c727116e25e36b75374569
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2025-01-12 13:04:41 +0400

    update app configuration and add main page with login form

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
 import express from 'express';
 
-
-
-
-
 const app = express();
-const port = process.env.PR_PORT || 3000;
-
+const port = process.env.PR_PORT || 3500;
 
 app.set('view engine', 'ejs');
-app.set('views', './src/Views');
-
+app.set('views', './src');
 
 app.use(express.json());
-app.use('/', routes);
-app.use((req, res, next) => { res.status(404).send('404 Not Found'); next(); });
 
-app.use(async (err, req, res, next) => {
-    const errorData = {
-        title: 'Global Error',
-        text: `${err.stack}`
-    };
-console.log(errorData);
-    res.status(500).send('Something went wrong!');
+app.get('/', (req, res) => {
+    res.render('main', { title: 'Main Page', message: 'Welcome to the Main Page!' });
 });
 
+app.use((req, res) => {
+    res.status(404).send('404 Not Found');
+});
 
+app.use((err, req, res, next) => {
+    console.error('Error:', err.stack);
+    res.status(500).render('error', {
+        title: 'Global Error',
+        text: `${err.stack}`
+    });
+});
 
-
-app.listen(port, '0.0.0.0', () => { console.log(`Server is running at http://localhost:${port}`); });
-
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
